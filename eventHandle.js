7bfbb246c4a7fc05795542b5dbe14135f3e9dd12
D.Stein <rckstrongo@gmail.com>
2023-10-12 17:50:41 -0400

    Multiple changes
    Changed the display of the address column so that it scrolls with additional content rather than strechting neighboring content
    Changed the animation of the form collapsing
    Changed the grid layout so that labels do not need to be explicitly placed
    Added birthday field to the form

--- eventHandle.js
+++ eventHandle.js
@@ -1,6 +1,28 @@
+/*
 const form = document.getElementById("myForm");
 
 function showForm()
 {
     form.style.transform = 'scaleY(1)';
+}
+*/
+
+const collapse = document.getElementsByClassName("collapse");
+
+for (let ii = 0; ii < collapse.length; ii++)
+{
+    collapse[ii].addEventListener("click", function(e)
+        {
+            const content = this.nextElementSibling;
+            if(content.style.maxHeight)
+            {
+                content.style.maxHeight = null;
+                content.style.padding = null;
+            }
+            else
+            {
+                content.style.padding = content.getAttribute("padding");
+                content.style.maxHeight = content.scrollHeight + "px";
+            }
+        })
 }
--- readme.md
+++ readme.md
@@ -8,3 +8,5 @@ A sign-up form for an imaginary service. Created as a part of the ODIN Project c
 Carribean image: https://sostrenews.com/wp-content/uploads/2016/06/St-Lucia-Caribbean-Sea.jpg
 
 Parchment background texture: http://www.photos-public-domain.com/wp-content/uploads/2011/03/golden-parchment-paper-texture.jpg
+
+<a target="_blank" href="https://icons8.com/icon/0a0piUCBEAAa/poison">Pirate</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
